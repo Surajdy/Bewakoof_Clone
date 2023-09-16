65279b6865a68e100af65bd8fa96dd82ea84905e
ABHAY-22 <51116785+ABHAY-22@users.noreply.github.com>
2023-09-16 19:35:02 +0530

    product

---
+++ product_accessories_page.js
@@ -0,0 +1,340 @@
+var fashionData = [
+    {
+      avatar:
+        "https://images.bewakoof.com/t640/serpentine-premium-glass-case-for-oneplus-nord-n20-se-shock-proof-scratch-resistant-619136-1694147521-1.jpg",
+        brand : "Qrioh",
+      name: "One plus Nord N20",
+      rupees:"₹",
+      price: 599,
+      rupees2:"₹",
+      price2: 1499,
+      offer : "₹499 For TriBe Members"
+     
+       },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/skeleton-inside-premium-glass-case-for-oneplus-nord-n20-se-shock-proof-scratch-resistant-619247-1694143218-1.jpg",
+       
+        brand : "Qrioh",
+      name: "Skeleton Inside One plus Nord N20 ",
+      rupees:"₹",
+      price: 599,
+      rupees2:"₹",
+      price2: 1499,
+      offer : "₹499 For TriBe Members",
+      
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/shanks-luffy-premium-glass-case-for-oneplus-nord-n20-se-shock-proof-scratch-resistant-619188-1694123690-1.jpg",
+        brand : "Qrioh",
+      name: "Shanks & Luffy Premium Glass Case for OnePlus Nord N20 S",
+      rupees:"₹",
+      price: 529,
+      rupees2:"₹",
+      price2: 1529,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/smile-for-camera-premium-glass-case-for-oneplus-nord-n20-se-shock-proof-scratch-resistant-619356-1694121935-1.jpg",
+        brand : "Qrioh",
+      name: "Smile for camera Premium Glass Case",
+      rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 1299,
+      offer : "₹469 For TriBe Members"
+    
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/orange-chubby-premium-glass-case-for-oneplus-8-shock-proof-scratch-resistant-600065-1687441070-1.jpg",
+        brand : "Qrioh",
+        name: "Orange Chubby Premium Glass Case",
+        rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 1999,
+      offer : "₹399 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/pumped-up-dragon-premium-glass-case-for-oneplus-8-shock-proof-scratch-resistant-600223-1687443389-1.jpg",
+        brand : "Qrioh",
+        name: "Pumped Up Dragon Premium Glass Case ",
+        rupees:"₹",
+      price: 599,
+      rupees2:"₹",
+      price2: 1399,
+      offer : "₹399 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/old-soul-aesthetic-premium-glass-case-for-oneplus-8-609310-1691686738-1.jpg",
+        brand : "The Case Company",
+        name: "Old soul aesthetic premium glass case",
+        rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 999,
+      offer : "₹349 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/worship-printed-premium-glass-cover-for-s21-fe-5g-impact-resistant-matte-finish-533366-1659614394-1.jpg",
+        brand : "Qrioh",
+        name: "Abstract Faces Printed Premium Glass Cover",
+        rupees:"₹",
+      price: 449,
+      rupees2:"₹",
+      price2: 799,
+      offer : "₹469 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/angry-printed-premium-glass-cover-for-samsung-galaxy-s10-lite-impact-resistant-matte-finish-532491-1659617586-1.jpg",
+        brand : "Qrioh",
+        name: "Angry Printed Premium Glass Cover",
+        rupees:"₹",
+      price: 399,
+      rupees2:"₹",
+      price2: 1399,
+      offer : "₹439 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/skeleton-printed-premium-glass-cover-for-a23-impact-resistant-matte-finish-533596-1659617435-1.jpg",
+        brand : "Bewakoof",
+        name: "Skeleton Printed Premium Glass Cover for A23",
+        rupees:"₹",
+      price: 399,
+      rupees2:"₹",
+      price2: 1099,
+      offer : "₹379 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/do-not-disturb-typography-premium-glass-cover-for-a23-impact-resistant-matte-finish-533599-1659627734-1.jpg",
+        brand : "Qrioh",
+        name: "Do Not Disturb Typography Premium Glass Cover",
+        rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 999,
+      offer : "₹379 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/owl-art-printed-premium-glass-cover-for-samsung-galaxy-s21-ultra-impact-resistant-matte-finish-532405-1659537221-1.jpg",
+        brand : "Qrioh",
+        name: "Owl Art Printed Premium Glass Cover For Samsung Galaxy S2",
+        rupees:"₹",
+      price: 449,
+      rupees2:"₹",
+      price2: 999,
+      offer : "₹429 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/kitten-mandala-printed-premium-glass-cover-for-s21-ultra-impact-resistant-532633-1659616652-1.jpg",
+        brand : "Qrioh",
+        name: "Kitten Mandala Printed Premium Glass Cover For S21",
+        rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 699,
+      offer : "₹299 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/printed-premium-glass-cover-for-s21-ultra-impact-resistant-matte-finish-533873-1659615874-1.jpg",
+        brand : "Qrioh",
+        name: "Printed Premium Glass Cover For S21 Ultr",
+        rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 799,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/funny-pug-face-printed-premium-glass-cover-for-s21-ultra-impact-resistant-532626-1659615306-1.jpg",
+        brand : "Qrioh",
+        name: "Funny Pug Face Printed Premium Glass Cover For S21",
+        rupees:"₹",
+      price: 399,
+      rupees2:"₹",
+      price2: 599,
+      offer : "₹279 For TriBe Members"
+    },
+    
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/doomsday-titan-designer-hard-cover-for-realme-7-578192-1675317960-1.jpg",
+        brand : "Bewakoof",
+        name: "Doomsday Titan Designer Hard Cove",
+        rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 449,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t320/retro-art-printed-premium-glass-cover-for-realme-9i-shock-proof-lightweight-486334-1649163989-1.jpg",
+        brand : "Qrioh",
+        name: "Retro Art Printed Premium Glass Cover for Realme 9i",
+        rupees:"₹",
+      price: 499,
+      rupees2:"₹",
+      price2: 1499,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/responsibilities-tom-and-jerry-designer-hard-cover-for-realme-7-572155-1674626666-1.jpg",
+         brand : "Bewakoof",
+         name: "Responsibilities Tom And Jerry Designer Hard Cover",
+         rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 790,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/khonshu-moon-knight-designer-hard-cover-for-realme-7-567199-1672635973-1.jpg",
+        brand : "Bewakoof",
+        name: "Khonshu Moon Knight Designer Hard Cover",
+        rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 1299,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/dead-or-alive-premium-glass-case-for-huawei-p30-pro-shock-proof-scratch-resistant-573288-1674628155-1.jpg",
+        brand : "Qrioh",
+        name: "Dead alive glass for hawei",
+        rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 1299,
+      offer : "₹279 For TriBe Members"
+    },
+    {
+        avatar:
+        "https://images.bewakoof.com/t640/world-tour-designer-hard-cover-for-realme-7-539441-1662015925-1.jpg",
+        brand : "Bewakoof",
+        name: "World Tour Designer Hard Cover",
+        rupees:"₹",
+      price: 299,
+      rupees2:"₹",
+      price2: 899,
+      offer : "₹279 For TriBe Members"
+    },
+    
+    
+  ];
+
+  function handlePriceSort(){
+    var selected = document.querySelector("#sortPrice").value;
+    if(selected=="HTL"){
+      fashionData.sort(function(a,b){
+        return b.price - a.price;
+      });
+      displayData(fashionData)
+    }
+    if(selected=="LTH"){
+      fashionData.sort(function(a,b){
+        return a.price - b.price;
+      });
+      displayData(fashionData)
+    }
+    if(selected=="popular"){
+      fashionData.sort(function(a){
+        if(a.price>300)
+        {
+          return -1
+        }
+        if(a.price<300){
+          return 1;
+        }
+        else{
+          0;
+        }
+      });
+      displayData(fashionData)
+    }
+  }
+
+  //fashionArr=JSON.parse(localStorage.getItem("dataone"))||[];
+  
+displayData(fashionData)
+//console.log (fashionArr)
+
+
+
+function displayData(data){
+  document.querySelector("#container").innerHTML="";
+  fashionData.forEach(function (elem) {
+    //console.log (elem.name)
+    var box = document.createElement("div");
+    var fashion_avatar = document.createElement("img");
+    fashion_avatar.setAttribute("src", elem.avatar);
+    fashion_avatar.style.cursor="pointer";
+     var  fashion_brand = document.createElement("h3");
+    fashion_brand.innerText = elem.brand;
+    var fashion_name = document.createElement("p");
+    fashion_name.innerText = elem.name;
+
+    var div5 = document.createElement("div");
+    var fashion_rupees = document.createElement("h3");
+    fashion_rupees.innerText = elem.rupees;
+    fashion_rupees.style.color = "black";
+    div5.append(fashion_rupees)
+
+    var div1 = document.createElement("div");
+    var fashion_price = document.createElement("h3");
+    fashion_price.innerText = elem.price;
+    fashion_price.style.color = "black";
+    div1.append(fashion_price)
+
+    var div6 = document.createElement("div");
+    var fashion_rupees2 = document.createElement("h4");
+    fashion_rupees2.innerText = elem.rupees2;
+    div6.append(fashion_rupees2)
+
+    var fashion_price2 = document.createElement("h4");
+    fashion_price2.innerText = elem.price2;
+    var div2 = document.createElement("div");
+    div2.append(fashion_price2)
+
+    var div3 = document.createElement("div");
+    div3.append(div5,div1,div6,div2);
+
+    
+    // fashion_price2.style.color = "red"
+    var fashion_offer = document.createElement("h4");
+    fashion_offer.innerText = elem.offer;
+  
+    fashion_avatar.addEventListener("click",function(){
+            press(elem);
+        })
+    box.append(fashion_avatar, fashion_brand, fashion_name, div3, fashion_offer);
+    document.querySelector("#container").append(box);
+    //console.log (elem)
+
+  })
+}
+function press(elem){
+  document.querySelector("#container").innerHTML=""
+ // fashionArr.push(elem);
+ var product=[];
+ product.push(elem);
+ console.log(product);
+ localStorage.setItem("dataone", JSON.stringify(product));
+  window.location.href="product_cart.html"
+}  
---
+++ product_men_page.js
@@ -0,0 +1,472 @@
+var fashionData = [
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/killmonger-half-sleeve-t-shirt-509349-1655101044-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Olive Killmonger Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"S"
+
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t640/moon-rider-half-sleeve-t-shirt-475032-1655749540-1.jpg",
+
+    brand: "Bewakoof",
+    name: "Men's Red Moon Rider Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 399,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹469 For TriBe Members",
+    category: "T-Shirt",
+    size:"S"
+
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/men-s-black-kalyug-age-of-machines-graphic-printed-t-shirt-509633-1655211307-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Black Kalyug Age of Machines Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"L"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/time-flies-oversized-fit-t-shirt-509134-1654873540-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's White Time Flies Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 499,
+    rupees2: "₹",
+    price2: 1299,
+    offer: "₹469 For TriBe Members",
+    category: "T-Shirt",
+    size:"XL"
+
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/bugs-bunny-circle-half-sleeve-t-shirt-499256-1652767566-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 399,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹379 For TriBe Members",
+    category: "Vest",
+    size:"S"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/black-full-sleeve-henley-t-shirt-405279-1642406816-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Olive Killmonger Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 499,
+    rupees2: "₹",
+    price2: 830,
+    offer: "₹459 For TriBe Members",
+    category: "Vest",
+    size:"XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/customizable-black-printed-round-neck-t-shirt-443960-1645623796-1.jpg",
+    brand: "Breakbounce",
+    name: "Men's Olive Killmonger Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 499,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹449 For TriBe Members",
+    category: "Vest",
+    size:"4XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/need-space-teddy-half-sleeve-t-shirt-496045-1651841323-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Black I Need Some Space Teddy Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 449,
+    rupees2: "₹",
+    price2: 799,
+    offer: "₹419 For TriBe Members",
+    category: "T-Shirt",
+    size:"4XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/gaming-revolution-half-sleeve-t-shirt-black-331783-1635870712-5.jpg",
+    brand: "Difference_Of_Opinion",
+    name: "Gaming Revolution Half Sleeve T-Shirt Black",
+    rupees: "₹",
+    price: 499,
+    rupees2: "₹",
+    price2: 1299,
+    offer: "₹459 For TriBe Members",
+    category: "T-Shirt",
+    size:"4XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/the-traveller-full-sleeve-t-shirt-276528-1638212427-1.jpg",
+    brand: "Bewakoof",
+    name: "The Traveller Full Sleeve T-Shirt",
+    rupees: "₹",
+    price: 399,
+    rupees2: "₹",
+    price2: 1099,
+    offer: "₹379 For TriBe Members",
+    category: "T-Shirt",
+    size:"XL"
+
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/white-half-sleeve-t-shirt-105-1653031889-1.jpg",
+    brand: "Bewakoof",
+    name: "White Half Sleeve T-Shirt",
+    rupees: "₹",
+    price: 349,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹329 For TriBe Members",
+    category: "T-Shirt",
+    size:"L"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/hang-loose-relax-half-sleeve-t-shirt-387407-1640087754-5.jpg",
+    brand: "Bewakoof",
+    name: "Men's Black Relax T-shirt",
+    rupees: "₹",
+    price: 349,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹329 For TriBe Members",
+    category: "T-Shirt",
+    size:"S"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/marvel-s-moon-knight-half-sleeve-t-shirt-483814-1648618777-1.jpg",
+    brand: "Alstyle",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 1099,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"2XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/jet-black-half-sleeve-t-shirt-106-1637159826-5.jpg",
+    brand: "Bewakoof",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 1299,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"28"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/men-s-black-winter-soldier-logo-oversized-t-shirt-466321-1649266565-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 999,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"32"
+  },
+
+  {
+    avatar:
+      "https://images.bewakoof.com/t320/marvel-s-moon-knight-half-sleeve-t-shirt-483825-1648618827-1.jpg",
+    brand: "Dillinger",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 1249,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"30"
+
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t640/men-s-green-fight-the-joker-graphic-printed-oversized-t-shirt-620075-1694518975-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Blue Bugs Bunny Circle Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 299,
+    rupees2: "₹",
+    price2: 1099,
+    offer: "₹279 For TriBe Members",
+    category: "T-Shirt",
+    size:"XS"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t1080/men-s-black-feel-most-alive-t-shirt-307093-1655749411-1.jpg",
+    brand: "Snith",
+    name: "Men's Black Feel Most Alive Graphic Printed T-shirt",
+    rupees: "₹",
+    price: 499,
+    rupees2: "₹",
+    price2: 830,
+    offer: "₹379 For TriBe Members",
+    category: "T-Shirt",
+    size:"S"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t640/men-s-blue-pirate-hunter-zoro-graphic-printed-oversized-t-shirt-620135-1694500304-1.jpg",
+    brand: "Bewakoof",
+    name: "Men's Blue Pirate Hunter Zoro Printed Oversized T-Shirt",
+    rupees: "₹",
+    price: 699,
+    rupees2: "₹",
+    price2: 1299,
+    offer: "639 For TriBe Members",
+    category:"T-Shirt",
+    size:"XL"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t640/men-s-blue-gotham-city-guardian-graphic-printed-oversized-t-shirt-620073-1694407332-1.jpg",
+    brand: "Hangup",
+    name: "Men's Blue Gotham City Guardian Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 649,
+    rupees2: "₹",
+    price2: 1299,
+    offer: "₹599 For TriBe Members",
+    category: "T-Shirt",
+    size:"L"
+  },
+  {
+    avatar:
+      "https://images.bewakoof.com/t640/men-s-black-across-the-spiderverse-graphic-printed-oversized-t-shirt-599566-1687765259-1.jpg",
+    brand: "Alcis",
+    name: "Men's Black Across The Spiderverse Graphic Printed Oversized T-shirt",
+    rupees: "₹",
+    price: 699,
+    rupees2: "₹",
+    price2: 1099,
+    offer: "₹639 For TriBe Members" ,
+    category: "T-Shirt",
+    size:"M",
+    color:"black"
+  },
+
+
+];
+function handlePriceSort() {
+  var selected = document.querySelector("#sortPrice").value;
+  if (selected == "HTL") {
+    fashionData.sort(function (a, b) {
+      return b.price - a.price;
+    });
+    displayData(fashionData)
+  }
+  if (selected == "LTH") {
+    fashionData.sort(function (a, b) {
+      return a.price - b.price;
+    });
+    displayData(fashionData)
+  }
+  if(selected=="popular"){
+    fashionData.sort(function(a){
+      if(a.price>300)
+      {
+        return -1
+      }
+      if(a.price<300){
+        return 1;
+      }
+      else{
+        0;
+      }
+    });
+    displayData(fashionData)
+  }
+}
+
+//fashionArr=JSON.parse(localStorage.getItem("dataone"))||[];
+
+displayData(fashionData)
+//console.log (fashionArr)
+
+function handleFilter() {
+  var selectedCategory = document.querySelector("#filterVenue").value;
+  
+  if (selectedCategory === "") {
+    // Show all products when "Category" is selected
+    displayData(fashionData);
+  } else {
+    // Filter fashionData based on the selected category
+    var filteredData = fashionData.filter(function (item) {
+      return item.category.toLowerCase() === selectedCategory.toLowerCase();
+    });
+    displayData(filteredData);
+  }
+}
+
+var selectVenue = document.querySelector("#Venue");
+
+// Add an event listener to the select element for sizes
+selectVenue.addEventListener("change", handleFilter1);
+
+function handleFilter1() {
+  var selectedSize = document.querySelector("#Venue").value;
+
+  if (selectedSize === "") {
+    // Show all products when "Sizes" is selected
+    displayData(fashionData);
+  } else {
+    // Filter fashionData based on the selected size
+    var filteredData = fashionData.filter(function (item) {
+      return item.size && item.size.toLowerCase() === selectedSize.toLowerCase();
+    });
+    displayData(filteredData);
+  }
+}
+
+var selectBrand = document.querySelector("#Va");
+
+// Add an event listener to the select element for brand filtering
+selectBrand.addEventListener("change", handleBrandFilter);
+
+function handleBrandFilter() {
+  var selectedBrand = document.querySelector("#Va").value;
+
+  if (selectedBrand === "none") {
+    // Show all products when "Brand" is selected
+    displayData(fashionData);
+  } else {
+    // Filter fashionData based on the selected brand
+    var filteredData = fashionData.filter(function (item) {
+      return item.brand.toLowerCase() === selectedBrand.toLowerCase();
+    });
+    displayData(filteredData);
+  }
+}
+
+
+//color
+var selectColor = document.querySelector("#Vb");
+
+// Add an event listener to the select element for color filtering
+selectColor.addEventListener("change", handleColorFilter);
+
+function handleColorFilter() {
+  var selectedColor = document.querySelector("#Vb").value;
+
+  if (selectedColor === "none") {
+    // Show all products when "Color" is selected
+    displayData(fashionData);
+  } else {
+    // Filter fashionData based on the selected color
+    var filteredData = fashionData.filter(function (item) {
+      return item.color && item.color.toLowerCase() === selectedColor.toLowerCase();
+    });
+    displayData(filteredData);
+  }
+}
+
+
+
+
+
+
+
+
+
+function displayData(data) {
+  document.querySelector("#container").innerHTML = "";
+  data.forEach(function (elem) {
+    //console.log (elem.name)
+    var box = document.createElement("div");
+    var fashion_avatar = document.createElement("img");
+    fashion_avatar.setAttribute("src", elem.avatar);
+    fashion_avatar.style.cursor = "pointer";
+    var fashion_brand = document.createElement("h3");
+    fashion_brand.innerText = elem.brand;
+    var fashion_name = document.createElement("p");
+    fashion_name.innerText = elem.name;
+
+    var div5 = document.createElement("div");
+    var fashion_rupees = document.createElement("h3");
+    fashion_rupees.innerText = elem.rupees;
+    fashion_rupees.style.color = "black";
+    div5.append(fashion_rupees)
+
+    var div1 = document.createElement("div");
+    var fashion_price = document.createElement("h3");
+    fashion_price.innerText = elem.price;
+    fashion_price.style.color = "black";
+    div1.append(fashion_price)
+
+    var div6 = document.createElement("div");
+    var fashion_rupees2 = document.createElement("h4");
+    fashion_rupees2.innerText = elem.rupees2;
+    div6.append(fashion_rupees2)
+
+    var fashion_price2 = document.createElement("h4");
+    fashion_price2.innerText = elem.price2;
+    var div2 = document.createElement("div");
+    div2.append(fashion_price2)
+
+    var div3 = document.createElement("div");
+    div3.append(div5, div1, div6, div2);
+
+    var fashion_offer = document.createElement("h4");
+    fashion_offer.innerText = elem.offer;
+
+    fashion_avatar.addEventListener("click", function () {
+      press(elem);
+    })
+    box.append(fashion_avatar, fashion_brand, fashion_name, div3, fashion_offer);
+    document.querySelector("#container").append(box);
+    //console.log (elem)
+
+  })
+}
+function press(elem) {
+  document.querySelector("#container").innerHTML = ""
+  // fashionArr.push(elem);
+  var product = [];
+  product.push(elem)
+  console.log(product)
+  localStorage.setItem("dataone", JSON.stringify(product));
+  window.location.href = "product_cart.html";
+}
+let userdata=JSON.parse(localStorage.getItem('user'));
+// console.log(userdata.username);
+if(userdata!=null){
+document.querySelector("#username").innerText=`Hi, ${userdata.username}`;
+document.querySelector('#signin').style.display="none"
+document.querySelector('#heart').style.display="none"
+}
